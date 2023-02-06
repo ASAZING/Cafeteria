2cdf85da0dd49281f1c2348caac1fcce3df7cb19
ASAZING <malcolmedina206@gmail.com>
2023-02-06 12:42:34 -0500

    Creo vista para mensaje cuando se elimian un producto

---
+++ Views/delete_product.php
@@ -0,0 +1,5 @@
+<div class="row">
+	<div class="alert alert-success">
+		Producto eliminada correctamente. <a href="index.php?controller=productcontroller&action=list">Volver al listado</a>
+	</div>
+</div>
