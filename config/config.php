a9c4e65c72ab54803ff581fb3b54b169b14d372d
ASAZING <malcolmedina206@gmail.com>
2023-02-06 09:33:31 -0500

    Initial commit

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+/* Database connection values */
+define("DB_HOST", "localhost");
+define("DB", "cafeteria");
+define("DB_USER", "root");
+define("DB_PASS", "");
+
+/* Default options */
+define("DEFAULT_CONTROLLER", "ProductController");
+define("DEFAULT_ACTION", "list");
